d74f073708d63f03ce696ebdf45e8a0d4f6e63d7
johannakate <johanna@ripple.com>
2016-04-28 19:33:47 -0700

    more changes

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
-const http = require('http');
-const fs = require('fs');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 // const graphJS = require('./forcedGraph').graphThis();
-const miserables = require('./miserables.json');
+var miserables = require('./data/miserables.json');
 
-var server = http.createServer( (request, response) => {
+var server = http.createServer( function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
-  var file = 'forced_graph_les_miserables.html';
-  var html = fs.readFileSync(file);
+  console.log(miserables);
+  console.log(html);
+  var filename = req.url || "forced_graph_les_miserables.html";
+  console.log(filename);
+  var ext = path.extname(filename);
+  console.log(filename);
+  var localPath = __dirname;
+	var validExtensions = {
+		".html" : "text/html",			
+		".js": "application/javascript", 
+		".css": "text/css",
+		".txt": "text/plain",
+		".jpg": "image/jpeg",
+		".gif": "image/gif",
+		".png": "image/png"
+	};
+	var isValidExt = validExtensions[ext];
+
+	if (isValidExt) {
+		
+		localPath += filename;
+		path.exists(localPath, function(exists) {
+			if(exists) {
+				console.log("Serving file: " + localPath);
+				getFile(localPath, res, ext);
+			} else {
+				console.log("File not found: " + localPath);
+				res.writeHead(404);
+				res.end();
+			}
+		});
+
+	} else {
+		console.log("Invalid file extension detected: " + ext);
+	}
   response.end(html, 'binary');
-}).listen(1337, function () {
+}).listen(1337, "localhost", function () {
+	var address = server.address();
 	var host = server.address().address;
 	var port = server.address().port;
-	console.log('App listening at http://%s:%s', host, port);
+	console.log('App listening at http://%j:%j', host, port);
 });
 
