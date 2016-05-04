7309f92bb3703d2c909fe578ed4131edd63748b4
johannakate <johanna@ripple.com>
2016-05-03 17:47:35 -0700

    response changes

--- app2.js
+++ app2.js
@@ -5,15 +5,21 @@ var miserables = require('./data/miserables.json');
 
 http.createServer(function(req, res) {
 
-	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.writeHead(200, { 'Content-Type': 'text/html' });
 	
 	var miserables = JSON.parse(fs.readFileSync("./data/miserables.json", 'utf8'));
-	var html = fs.readFile('./forced_graph_les_miserables.html', (err, data) => {
-		if (err !== null ) throw err;
-		console.log(data);
-		console.log(err);
+
+	var html = fs.readFile('./forced_graph_les_miserables.html', "utf8", (err, data) => {
+		
+		if (err) return res.end(err);
+
+		console.log("data:" + data);
+		res.end(data);
 	});
 
-	res.end();
-
 }).listen(1337, '127.0.0.1');
+
+// 1. hand json to html and render within html in <script> tags => put json as text into the markup 
+// 2. make an ajax call to server and have the server create a separate route to return JSON
+// 3. (not recommended) have the browser access the file directly - using web browser as a file server 
+// 		- how a person would access an image
